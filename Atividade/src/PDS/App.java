6ed2b11ce1765a583fd1f7b75f234b89ec988db8
Evelynmaria12 <maria.evelyn@escolar.ifrn.edu.br>
2024-12-16 17:59:55 -0300

    Projeto pronto

---
+++ Atividade/src/PDS/App.java
@@ -0,0 +1,11 @@
+package PDS;
+
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Evelyn Maria", 18);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+    }
+}
